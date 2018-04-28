4ffa6df112e98d3576efa35956a56b960ebba27b
Alex Calderwood <alexander.d.calderwood@gmail.com>
2018-04-28 08:03:22 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# All is all - Poetry Generation Engine
+
+1.) Download the CMU pronunciation dictionary at http://www.speech.cs.cmu.edu/cgi-bin/cmudict and extract it into the corpus/pronouncing-dict/ folder.
+
+2.)
+
+
---
+++ corpus/get_cmu.sh
@@ -0,0 +1,4 @@
+Bash script
+wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows  --no-parent http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict.0.7a.symbols
+
+
