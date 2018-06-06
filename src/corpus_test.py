459e6ab2a48fcac2058d40c11b29c800ece66287
Alex <alexc@production.pro>
2018-06-06 14:39:49 -0400

    Added test to corpus iteration.

---
+++ src/corpus_test.py
@@ -0,0 +1,15 @@
+import unittest
+from src.markov import Corpus
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_iter(self):
+        corpus = Corpus('testcorpus.txt')
+        print(corpus)
+
+        for s in corpus:
+            for i in s:
+                print(i)
+
+if __name__ == '__main__':
+    unittest.main()
