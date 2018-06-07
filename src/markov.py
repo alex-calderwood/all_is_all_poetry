7763256c77d966fa6e1a4f1f567104020344fb50
Alex <alexc@production.pro>
2018-06-07 17:16:28 -0400

    Finished ngram counting.

--- src/markov.py
+++ src/markov.py
@@ -2,19 +2,18 @@ import nltk
 from collections import Counter
 from src.corpus import  Corpus
 
-END = '<END>'
-PREFIX = '<PRE-{}>'
 
-class Token():
+class PoemEngine():
+    def train(self):
+        pass
 
-    def __init__(self, features):
-        self.n = len(features)
-        self.features = features
+    def predict(self):
+        pass
 
 
-class MarkovChain():
+class MarkovChain(PoemEngine):
     
-    def __init__(self, corpus, N):
+    def __init__(self, N):
         """
         Reads a corpus, a list of sentences. A sentence is a list of tokens. Each token is of an arbitrary number of features.
         :param N: the length of the markov chain, e.g. 2. 
@@ -22,31 +21,54 @@ class MarkovChain():
 
         self.N = N
 
-        self.ngrams = self.__count_ngrams(corpus)
 
-        for thing in self.ngrams:
-            print(thing)
+    def train(self, corpus):
+        self.ngrams = self.count_ngrams(corpus)
 
-    def __count_ngrams(self, corpus):
+
+    def count_ngrams(self, corpus):
         """
-        Untested. Make ngrams from a corpus.
+        Make ngrams from a corpus.
         """
 
         ngrams = Counter()
 
-        # Make each ngram for n in {1..N}
-        for sequence in corpus:
-            for token_index in range(len(sequence) + 1):
-                # for n in reversed(range(1, self.N)):
-                for n in [1]:
-                    ngram = sequence[token_index - n: token_index]
-                    print(token_index - n, token_index, ngram)
-                    ngrams[tuple(ngram)] += 1
+        # Add each ngram for n in {1..N} to the counter
+        for n in range(1, self.N + 1):
+            self.__count_ngrams(n, ngrams, corpus)
 
         return ngrams
 
+    def __count_ngrams(self, n, ngrams, corpus):
+        """
+        For every new ngram seen, add it to the running count.
+        :param n: The current value of n.
+        :param ngrams: The running counter.
+        :param corpus: The corpus to count.
+        :return:
+        """
+
+        for sequence in corpus:
+            sequence_length = len(sequence.augmented())
+            for token_index in range(sequence_length):
+
+                if token_index + n > sequence_length:
+                    continue
+
+                # Get the current ngram
+                ngram = sequence.augmented()[token_index: token_index + n]
+
+                # Add the ngram to the running count
+                ngrams[tuple(ngram)] += 1
+
+    def print_counts(self):
+        for ngram, count in self.ngrams.items():
+            print(ngram, count)
+
 
 if __name__ == '__main__':
-    corpus = Corpus('testcorpus.txt')
-    mk = MarkovChain(corpus, 2)
+    corpus = Corpus('testcorpus.txt', 2)
+    mk = MarkovChain(2)
+    mk.train(corpus)
+    mk.print_counts()
 
