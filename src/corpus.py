1280076ba8a2401e6035fb1a493f3c12e8f0aab2
Alex <alexc@production.pro>
2018-06-07 17:13:13 -0400

    Refactored Corpus structure into list and augmented list.

--- src/corpus.py
+++ src/corpus.py
@@ -1,27 +1,52 @@
+# Written by Alex Calderwood on June 6th, 2018
+
+END = '<END>'
+PREFIX = '<PRE-{}>'
+
+class Token():
+
+    def __init__(self, features):
+        self.n = len(features)
+        self.features = features
+
 class Sequence():
-    def __init__(self, list):
-        self.__list = list
+    """
+    Represents a sequence of words or feature vectors. Can also be thought of as a sentence. Each sequence is augmented
+    by extra tokens on the beginning and end for use by a prediction algorithm.
+    """
+    def __init__(self, list, prefix_size=0):
+        """
+        Initialize a sequence object.
+        :param list: The list (of words, feature vectors, etc.) to be made into a sequence.
+        :param prefix_size: The size of the prefix sequence, i.e. the number of trailing prefix tokens (of the form '<PRE-{}>').
+        """
+        self.prefix = [PREFIX.format(i) for i in reversed(range(1, prefix_size + 1))]
+        self.list = list
+        self.postfix = [END]
 
     def __getitem__(self, key):
-        if isinstance(key, slice):
-            step = 1 if key.step is None else key.step
-            return [self.__getitem__(i) for i in range(key.start, key.stop, step)]
-
-        if key < 0:
-            return PREFIX.format(-key)
-        elif key == len(self.__list):
-            return END
-        else:
-            return self.__list[key]
+        return self.list[key]
 
     def __len__(self):
-        return len(self.__list)
+        return len(self.list)
+
+    def __str__(self):
+        return str(self.list)
+
+    def augmented(self):
+        """
+        :return: the sequence augmented by the prefix <and suffix
+        """
+        return self.prefix + self.list + self.postfix
+
+    def update_prefix_size(self, new_size):
+        self.prefix = [PREFIX.format(i) for i in reversed(range(1, new_size + 1))]
 
 
 class Corpus():
-    def __init__(self, filename):
+    def __init__(self, filename, prefix_padding_size=0):
         with open(filename, 'r') as f:
-            self.__sequences = [Sequence(line.strip().split(' ')) for line in f.readlines()]
+            self.__sequences = [Sequence(line.strip().split(' '), prefix_padding_size) for line in f.readlines()]
 
     def __iter__(self):
         return iter(self.__sequences)
