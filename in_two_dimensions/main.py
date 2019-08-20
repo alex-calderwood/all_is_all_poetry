9a0eb8b00b1e461a8b1463fd32bb9c2776bf0fd7
Alex <alexander.d.calderwood@gmail.com>
2019-08-20 13:47:22 -0400

    Moved app initialization to __init__.py

--- in_two_dimensions/main.py
+++ in_two_dimensions/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect
 import json
 from .grid import compose_grid
-app = Flask(__name__)
+from . import app
 
 previous_words = ['north', 'west', 'east', 'south']
 
